973a9d8f90c3579e808a5818dec1dffb4a670456
Figueiredo <figo.ppv@gmail.com>
2022-06-21 17:49:19 -0300

    enable CORS

--- src/server.ts
+++ src/server.ts
@@ -1,61 +1,63 @@
-import express, { response } from "express";
-import { v4 as uuid } from "uuid";
+import express, { response } from 'express'
+import { v4 as uuid } from 'uuid'
+import cors from 'cors'
 
-const app = express();
+const app = express()
 
-app.use(express.json());
+app.use(express.json())
+app.use(cors({ origin: '*' }))
 
 interface User {
-  id: string;
-  name: string;
-  email: string;
+    id: string
+    name: string
+    email: string
 }
 
-const users: User[] = [];
+const users: User[] = []
 
-app.get("/users", (req, res) => {
-  return res.json(users);
-});
+app.get('/users', (req, res) => {
+    return res.json(users)
+})
 
-app.post("/users", (req, res) => {
-  const { name, email } = req.body;
+app.post('/users', (req, res) => {
+    const { name, email } = req.body
 
-  const user = { id: uuid(), name, email };
+    const user = { id: uuid(), name, email }
 
-  users.push(user);
+    users.push(user)
 
-  return res.json(user);
-});
+    return res.json(user)
+})
 
-app.put("/users/:id", (req, res) => {
-  const { id } = req.params;
-  const { name, email } = req.body;
+app.put('/users/:id', (req, res) => {
+    const { id } = req.params
+    const { name, email } = req.body
 
-  const userIndex = users.findIndex((user) => user.id === id);
+    const userIndex = users.findIndex((user) => user.id === id)
 
-  if (userIndex < 0) {
-    return res.status(404).json({ error: "User not found" });
-  }
+    if (userIndex < 0) {
+        return res.status(404).json({ error: 'User not found' })
+    }
 
-  users[userIndex] = { id, name, email };
+    users[userIndex] = { id, name, email }
 
-  return res.json({ id, name, email });
-});
+    return res.json({ id, name, email })
+})
 
-app.delete("/users/:id", (req, res) => {
-  const { id } = req.params;
+app.delete('/users/:id', (req, res) => {
+    const { id } = req.params
 
-  const userIndex = users.findIndex((user) => user.id === id);
+    const userIndex = users.findIndex((user) => user.id === id)
 
-  if (userIndex < 0) {
-    return res.status(404).json({ error: "User not found" });
-  }
+    if (userIndex < 0) {
+        return res.status(404).json({ error: 'User not found' })
+    }
 
-  users.splice(userIndex, 1);
+    users.splice(userIndex, 1)
 
-  return res.status(204).send();
-});
+    return res.status(204).send()
+})
 
-app.listen("3333", () => {
-  console.log("Backend Started");
-});
+app.listen('3333', () => {
+    console.log('Backend Started')
+})
